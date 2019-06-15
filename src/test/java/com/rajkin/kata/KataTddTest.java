ddba67cc145d38c1c6f8b4e8a2d3b3890d4f8b87
rajkin <rajesh.kinkhabwala@rawalinfocom.com>
2019-06-16 01:27:20 +0530

    2.Allow the Add method to handle an unknown amount of numbers.

--- src/test/java/com/rajkin/kata/KataTddTest.java
+++ src/test/java/com/rajkin/kata/KataTddTest.java
@@ -24,4 +24,17 @@ public class KataTddTest {
         assertEquals(KataTdd.Add("7,27"), 34);
     }
 
+    //SecondIteration
+    //2.Allow the Add method to handle an unknown amount of numbers.
+    @Test
+    public void moreDigitsSupporte() {
+        assertEquals(KataTdd.Add("22,55,777"), 854);
+    }
+
+    @Test
+    public void unknownAmountOFNnumersAsInput() {
+        assertEquals(KataTdd.Add("1,2,3,44,5,6,8,9,07,999,1016"), 2100);
+    }
+
+
 }
